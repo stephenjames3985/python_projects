eecb21529357a2f44c75a4fa48c7fcb03ed5395f
Stephen Childs <stephenjameschilds@gmail.com>
2018-11-16 00:50:43 -0600

    Erase and start over for file

--- tar_archive.py
+++ tar_archive.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-import subprocess
+import tarfile
 import time
+import os
 
 def main():
     '''
@@ -13,25 +14,55 @@ def main():
     file: str
     '''
     # i might possibly make the next iteraton of this little
-    # extraction utility more interactive, but it is almost 2am, 
-    # and i am getting too tired to think clearly right now.
+    # extraction utility more interactive, and capable in the future,
+    # but it is almost 1am as i write this, and i am getting too
+    # tired to think clearly right now.
 
-    # variable declarations
+    # global variables
     datetime = time.strftime('%Y/%m/%d_%H:%M')
-    suffix = datetime + '.backup.tar.gz'
-    arg = sys.argv[1]
-    absolute_path = sys.argv[2]
-    path_split = absolute_path.split('/')
-    filename = pathpath_split[-1]
+    suffix = '.' + datetime + '.backup.tar.gz'
+    default_dir = os.getcwd()
+
+    # if there are arguments called with the script, just like calling a regular
+    # tar command order
+    if len(sys.argv) > 1:
+        # variable declarations
+        arg = sys.argv[1]
+        filename = sys.argv[2]
+        path = sys.argv[3]
+        # by default, the files will be *.tar.gz
+        tar = tarfile.open(name=filename + suffix, mode='x:gz')
+
+    # if there are no arguments given with the script
+    else:
+        # input variable creation
+        arg = input('Would you like to [c]ompress or e[x]tract a tar archive?\n>> ')
+        path = input('Please enter the (abs) path to the directory which contains the file(s)\nyou would like to compress or extract.\n>> ')
+        filename = input('Please enter the name you would like appended to the archive.\n>> ')
+        compression = input('Would you like to use compression type [gz], [bz2], or [xz]?\n>> ')
+        name = filename + datetime + '.tar.' + compression
+        tar = tarfile.open(name, mode='x:' + compression)
+
+    # define function to put us in the working directory
+    def ch_working_dir():
+        '''
+        when called, the function will change to the desired directory
+        '''
+        os.chdir(path)
+        files = os.listdir()
+
+    # define function for compression/creation of tarfile
+    def make_tar_file():
+        '''
+        this is the main function of this program, which is to create a tarfile
+        '''
+        # (until i am able to add more to it when i have more time and i am not also completely exhausted haha)
+
 
     # try/except clause in case any typos or unintended chars get entered
     try:
         # program logic for the archive process
-        if arg == 'z':
-            subprocess.Popen(['tar czvf', '~/Documents/' + filename + '.' + suffix, absolute_path])
-        # program logic for the unarchive process
-        elif arg == 'u':
-            subprocess.Popen(['tar xzvf', absolute_path])
+
         # hippity hoppity, easter's on its way!
         elif arg == 'hello' and absolute_path == 'world':
             print('You have found the magic easter egg! Make a wish, and the\ntoothe fairy will leave you a present under the christmas tree')
